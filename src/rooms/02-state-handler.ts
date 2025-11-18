bd41c3c01780499412ed7312081c4c60a6d302a6
Myakota89 <myakota89@yandex.ru>
2025-11-19 01:37:16 +0300

    Collect apples

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -1,27 +1,51 @@
 import { Room, Client } from "colyseus";
-import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
 
-class Vector2 {
-    x: number;
-    z: number;
+export class Vector2Float extends Schema {
+    @type("number") x: number;
+    @type("number") z: number;
 }
 
-const SPAWN_POINTS: Vector2[] = [{x:10, z:10}, {x:-10, z:10}, {x:10, z:-10}, {x:-10, z:-10}]
+export class Apple extends Schema {
+    @type("uint32") id;
+    @type(Vector2Float) position;
+}
 
 export class Player extends Schema {
     @type("number") speed = 0;
-    @type("uint16") p = 0;
-    @type("uint16") c = 0;
+    @type("uint16") p = 0; //parts
+    @type("uint16") s = 0; //score
+    @type("uint16") c = 0; //color
     @type("number") pX = 0;
     @type("number") pZ = 0;
 }
 
 export class State extends Schema {
-    @type({ map: Player })
-    players = new MapSchema<Player>();
+    @type({ map: Player }) players = new MapSchema<Player>();
+    @type([Apple]) applesData = new ArraySchema<Apple>();
+
     colorsLength: number;
     spawnPointsLength: number;
     colors: number[] = [];
+    fieldSize = 40;
+    appleId = 0;
+
+    createApple() {
+        const appleData = new Apple();
+        appleData.id = ++this.appleId;
+        appleData.position = this.getRandomFieldPoint();
+        this.applesData.push(appleData);
+    }
+
+    collectApple(player: Player, data: any) {
+        const apple = this.applesData.find(a => a.id === data.id);
+        if (apple === undefined)
+            return;
+
+        apple.position = this.getRandomFieldPoint();
+        player.s++;
+        player.p = Math.round(player.s / 3);
+    }
 
     createPlayer(sessionId: string, data: any) {
         const colorIdx = this.getPlayerColor();
@@ -32,9 +56,9 @@ export class State extends Schema {
         player.p = data.parts;
         player.c = colorIdx;
 
-        const spawnPosition = SPAWN_POINTS[this.players.size]
-        player.pX = spawnPosition.x;
-        player.pZ = spawnPosition.z;
+        const randomPoint = this.getRandomFieldPoint();
+        player.pX = randomPoint.x;
+        player.pZ = randomPoint.z;
 
         this.players.set(sessionId, player);
     }
@@ -59,10 +83,19 @@ export class State extends Schema {
         
         return 0;
     }
+
+    getRandomFieldPoint(): Vector2Float {
+        const point = new Vector2Float();
+        point.x = Math.floor(Math.random() * this.fieldSize) - this.fieldSize / 2;
+        point.z = Math.floor(Math.random() * this.fieldSize) - this.fieldSize / 2;
+
+        return point;
+    }
 }
 
 export class StateHandlerRoom extends Room<State> {
     maxClients = 4;
+    startAppleCount = 100;
 
     onCreate (options) {
         this.setState(new State());
@@ -71,6 +104,15 @@ export class StateHandlerRoom extends Room<State> {
         this.onMessage("move", (client, data) => {
             this.state.movePlayer(client.sessionId, data);
         });
+
+        this.onMessage("collect", (client, data) => {
+            const player = this.state.players.get(client.sessionId);
+            this.state.collectApple(player, data);
+        });
+
+        for (let i = 0; i < this.startAppleCount; i++) {
+            this.state.createApple();
+        }
     }
 
     onAuth(client, options, req) {
